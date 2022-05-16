d52ffc10a1443844222916bf242b0f3f2eb9ff4d
unknown <kkomang0622@gmail.com>
2022-05-16 15:49:31 +0900

    React

---
+++ ch06/src/components/Greeting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Greeting = () => {
+    
+    return (
+        <div className='Greeting' style={{backgroundColor:'yellow'}}>
+            <h4>greeting</h4>
+            <p>
+                greeting 컴포넌트 입니다.
+            </p>
+            
+        </div>
+    );
+};
+
+export default Greeting;
